4cc99dad18e50e63d7297b4083ab01ab8837f376
Jorge <jlramirez1210@gmail.com>
2025-05-06 23:47:26 -0500

    diseño GUI agregar

---
+++ src/AddEstablishment.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class AddEstablishment {
+    private JPanel ContentPane;
+    private JLabel lblCode;
+    private JTextField txtCode;
+    private JLabel lblUbigeo;
+    private JTextField textField1;
+    private JTextField txtRuc;
+    private JTextField txtName;
+    private JLabel lblRuc;
+    private JComboBox cmbType;
+    private JLabel lblType;
+    private JLabel lblName;
+    private JTextField txtAddress;
+    private JLabel lblAddress;
+    private JComboBox cmbState;
+    private JLabel lblState;
+    private JTextField txtEmail;
+    private JLabel lblEmail;
+    private JTextField txtPhone;
+    private JLabel lblPhone;
+    private JButton btnSave;
+}
